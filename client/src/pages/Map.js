078c7bc27523c87c3c7042cf4330c2ef38e1d8c2
CaroS <carolyn.mary@hotmail.com>
2021-01-23 20:18:48 -0500

    testing icon png

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -1,15 +1,9 @@
 import React from "react";
 import MapCom from "../components/MapCom/MapCom"
-import { Col, Row } from "../components/Grid";
 
 function Map() {
   return (
     <>
-    {/* <Row>
-      <Col size="md-12">
-        <h1>MAP</h1>
-      </Col>
-    </Row> */}
     <MapCom />
     </>
   );
